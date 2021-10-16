af6c23529e1f2426b1bb57be842831e28e74de23
Basketballforce <28318597+Basketballforce@users.noreply.github.com>
2021-10-16 00:36:51 -0400

    setup gulp purge

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
+// npm library dependencies.
 const {src,dest,watch,series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecssc = require('gulp-purgecss')
 
+// compile sass, purge un-used sass and generate css file
 function buildStyles(){
     return src('sassItUp/**/*.scss')
     .pipe(sass())
+    .pipe(purgecssc({content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
+// files to watch changes for to recall buildstyles function
 function watchMan() {
-    watch(['sassItUp/**/*.scss'],buildStyles)
+    watch(['sassItUp/**/*.scss', '*.html'],buildStyles)
 }
 
-
+// export build styles and watchman functions to run on gulp call
 exports.default = series(buildStyles,watchMan)
